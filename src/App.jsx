de12cfe9f624b6ee4b5b62e81cb1f877d3d581c5
GiantToJa <szymon.brachaczek.31@gmail.com>
2025-10-22 16:06:56 +0200

    skibidi

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,12 @@ function App() {
           <h3>About us</h3>
           <div className="team">
             <div className="member">
-              <img src="https://cdn.discordapp.com/attachments/756610372739530803/1429608092366147655/image.png?ex=68fa0d8a&is=68f8bc0a&hm=313d4cf5c9e2d1f297759552f6db4c89865ebd10072f3b6511f71cf547f291f4&" alt="Kurwo" />
+              <img src="assets/images/eryk.png" alt="Kurwo" />
               <h4>Eryk</h4>
               <p>Skills:</p>
             </div>
             <div className="member">
-              <img src="https://cdn.discordapp.com/attachments/756610372739530803/1429607394182430874/image.png?ex=68fa0ce3&is=68f8bb63&hm=65b760e46ad204afdd6fca0fd26e0aef7f6e3da62be31dceeaacfe94cb2ca7d9&" alt="Kolega" />
+              <img src="assets/images/giant.png" alt="Kolega" />
               <h4>Giant</h4>
               <p>Skills:</p>
             </div>
